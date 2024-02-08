93958d704200b8a085bca17b9981872061132271
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-09 02:20:21 +0700

    fix: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,28 +5,57 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Moon } from "lucide-react";
+import { useTheme } from "@/context/theme-provider";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   const resumeDownloadLink =
     "https://drive.google.com/file/d/1wfZwIMOPmWBtM64xjgq4HE556lktNbz5/view?usp=sharing";
 
+  const { theme, setTheme } = useTheme();
+
+  function handleTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  }
+
   return (
     <>
-      <div className=" w-full flex justify-between pb-6 pl-8 pt-4">
-        <Avatar>
+      <div className=" w-full flex justify-between pb-6 pl-8 pt-4 sticky z-0">
+        <Avatar
+          className="cursor-pointer"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
 
-        <div className="flex gap-8 text-sm pr-8 items-center">
+        <div className="flex gap-4 text-sm pr-8 items-center">
           <DropdownMenu>
-            <DropdownMenuTrigger className="w-fit h-fit border py-1 px-4 rounded-md">
-              <p className="text-base">Menu</p>
+            <DropdownMenuTrigger className="w-fit h-fit border-b-2 border-black py-1 px-2 dark:border-white">
+              <p className="text-base">Nav</p>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuItem>About</DropdownMenuItem>
-              <DropdownMenuItem>Experience</DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                About
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => {
+                  navigate("/experience");
+                }}
+              >
+                Experience
+              </DropdownMenuItem>
               <DropdownMenuItem>Education</DropdownMenuItem>
               <DropdownMenuItem>Project</DropdownMenuItem>
               <DropdownMenuItem>Contact</DropdownMenuItem>
@@ -40,7 +69,13 @@ const Navbar = () => {
               </a>
             </DropdownMenuContent>
           </DropdownMenu>
-          <Moon />
+          <Button
+            onClick={() => handleTheme()}
+            variant={"outline"}
+            className="border-none"
+          >
+            {theme === "light" ? <Sun /> : <Moon />}
+          </Button>
         </div>
       </div>
     </>
