5b0afd61bc4d4a31a7239cde750a64693fc4102c
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-09 21:41:37 +0700

    fix: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,12 @@ const Home = () => {
   return (
     <>
       <Layout>
-        <div className="flex gap-24 pl-10 pt-16">
+        <div className="flex sm:gap-20 md:gap-12 lg:gap-12 xl:gap-24 md:pl-6 lg:pl-8 xl:pl-10 pt-16">
           <div className="flex flex-col gap-8 items-center">
-            <Separator orientation="vertical" className="h-24 bg-black" />
+            <Separator
+              orientation="vertical"
+              className="h-24 bg-black dark:bg-white"
+            />
             <a href={twitterLink} target="_blank" rel="noopener noreferrer">
               <Twitter />
             </a>
@@ -23,12 +26,19 @@ const Home = () => {
               <Github />
             </a>
 
-            <Separator orientation="vertical" className="h-24 bg-black" />
+            <Separator
+              orientation="vertical"
+              className="h-24 bg-black dark:bg-white"
+            />
           </div>
           <div className="flex flex-col gap-10">
-            <h1 className=" text-8xl">Bonjour</h1>
+            <h1 className="sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+              Bonjour
+            </h1>
             <div className="flex flex-col gap-4">
-              <p className=" text-5xl">I'm Gufran Lazuardi</p>
+              <p className="md:text3xl lg:text-4xl xl:text-5xl">
+                I'm Gufran Lazuardi
+              </p>
               <p className=" text-4xl">Frontend Developer</p>
             </div>
           </div>
