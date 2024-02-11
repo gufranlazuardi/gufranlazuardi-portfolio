d0f86362f497e714316e257f97a3a8fe83b97280
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-11 18:06:20 +0700

    fix: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <>
       <Layout>
-        <div className="flex sm:gap-20 md:gap-12 lg:gap-12 xl:gap-24 md:pl-6 lg:pl-8 xl:pl-10 pt-16">
+        <div className="flex sm:gap-20 md:gap-12 lg:gap-12 xl:gap-24 sm:pl-4 md:pl-6 lg:pl-8 xl:pl-10 pt-16">
           <div className="flex flex-col gap-8 items-center">
             <Separator
               orientation="vertical"
@@ -32,14 +32,35 @@ const Home = () => {
             />
           </div>
           <div className="flex flex-col gap-10">
-            <h1 className="sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
               Bonjour
             </h1>
             <div className="flex flex-col gap-4">
-              <p className="md:text3xl lg:text-4xl xl:text-5xl">
+              <p className="text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
                 I'm Gufran Lazuardi
               </p>
-              <p className=" text-4xl">Frontend Developer</p>
+              <span className="inline-flex flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden">
+                <ul className="block text-base sm:text-3xl leading-tight animate-text-slide">
+                  <li className="text-[#2f7df4] text-xl sm:text-3xl font-bold">
+                    Frontend Developer
+                  </li>
+                  <li className="text-[#2f7df4] text-xl sm:text-3xl font-bold">
+                    Gamer
+                  </li>
+                  <li className="text-[#2f7df4] text-xl sm:text-3xl font-bold">
+                    Runner
+                  </li>
+                  <li className="text-[#2f7df4] text-xl sm:text-3xl font-bold">
+                    Frontend Developer
+                  </li>
+                  <li className="text-[#2f7df4] text-xl sm:text-3xl font-bold">
+                    Gamer
+                  </li>
+                  <li className="text-[#2f7df4] text-xl sm:text-3xl font-bold">
+                    Runner
+                  </li>
+                </ul>
+              </span>
             </div>
           </div>
         </div>
